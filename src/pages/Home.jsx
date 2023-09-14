343268aa177add6185b4985560943b2bae3a0708
cicadaXiR <89849656+cicadaXiR@users.noreply.github.com>
2023-09-14 15:15:28 +0530

    modifications in home.jsx and added new model

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,17 @@ import React, { useState } from 'react'
 import "../Css/home.css"
 import { Link } from "react-router-dom";
 import { Navigate } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Home() {
-  const [loggedOut,setLoggedOut] = useState(false)
+  const [loggedOut, setLoggedOut] = useState(false)
 
-  const handleLoggedOut = () =>{
+  const handleLoggedOut = () => {
     setLoggedOut(true);
   };
 
-  if(loggedOut){
-      return <Navigate to="/"/>
+  if (loggedOut) {
+    return <Navigate to="/" />
   }
   return (
     <>
@@ -38,11 +39,36 @@ function Home() {
                 <Link className="navbar-brand mx-5" to="/flight">FightTicket</Link>
               </li>
               <li className="nav-item">
-                <input type="search" class="form-control" id="datatable-search-input" placeholder="search" style={{width:'250px'}}/>
+                <input type="search" class="form-control" id="datatable-search-input" placeholder="search" style={{ width: '250px' }} />
               </li>
+
               <li className="nav-item">
-                  <button className='btn btn-danger' onClick={handleLoggedOut}>logout</button>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                  profile
+                </button>
+
+                <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                      <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">profile</h5>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      <div className="modal-body">
+                        user Informations
+                      </div>
+                      <div className="modal-footer">
+                        <button type="button" className="btn btn-danger" data-dismiss="modal" onClick={handleLoggedOut}>Logout</button>
+                        <button type="button" className="btn btn-primary">Edit</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
               </li>
+
             </ul>
           </div>
         </nav>
